bb8f0b19bb308117562d26dad6cacb72421c5276
Cassio C <LccassioL@gmail.com>
2020-06-11 19:07:40 -0300

    Few adjusts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -20,6 +20,7 @@ import {
 
 const Home = () => {
   const [search, setSearch] = useState('');
+  const [showDetails, setShowDetails] = useState(false);
 
   const searchWeather = useCallback(() => {
     return weatherAPI.get('forecast', {
@@ -33,7 +34,7 @@ const Home = () => {
   }, [search]);
 
   const loadDefault = useCallback((city: string) => {
-    return weatherAPI.get('forecast', {
+    return weatherAPI.get('weather', {
       params: {
         q: city,
         appid: '93bbe3ad2f8c6d1617d79ab06f4cc5d0',
@@ -42,6 +43,33 @@ const Home = () => {
       },
     });
   }, []);
+
+  const handleSearch = useCallback(async () => {
+    console.log('search new weather');
+    console.log(search);
+
+    const { data } = await weatherAPI.get('forecast', {
+      params: {
+        q: `${search},BR`,
+        appid: '93bbe3ad2f8c6d1617d79ab06f4cc5d0',
+        units: 'metric',
+        lang: 'pt_br',
+      },
+    });
+
+    console.log(data);
+    // percorrer a lista que vem, para pegar uma vez de cada dia,
+    // para poder completar 5 dias
+  }, [search]);
+
+  const handleSearchKeyPress = useCallback(
+    (event: React.KeyboardEvent) => {
+      if (event.key === 'Enter') {
+        handleSearch();
+      }
+    },
+    [handleSearch]
+  );
 
   useEffect(() => {
     async function initialValues() {
@@ -52,8 +80,6 @@ const Home = () => {
         responseBelem,
         responseSalvador,
         responseCuritiba,
-        responseManaus,
-        responseJoaoPessoa,
       ] = await Promise.all([
         loadDefault('Recife,BR'),
         loadDefault('São Paulo,BR'),
@@ -61,8 +87,6 @@ const Home = () => {
         loadDefault('Belem,BR'),
         loadDefault('Salvador,BR'),
         loadDefault('Curitiba,BR'),
-        loadDefault('Manaus,BR'),
-        loadDefault('João Pessoa,BR'),
       ]);
     }
 
@@ -72,91 +96,95 @@ const Home = () => {
   return (
     <Container>
       <Title>Previsão do Tempo</Title>
-      <Details>
-        <City>
-          <span>Niterói, RJ - Brasil</span>
-          <button type="button">X</button>
-        </City>
-        <p>20℃ Nublado</p>
+      {showDetails && (
+        <Details>
+          <City>
+            <span>Niterói, RJ - Brasil</span>
+            <button type="button" onClick={() => setShowDetails(false)}>
+              X
+            </button>
+          </City>
+          <p>20℃ Nublado</p>
 
-        <MaxMinFeelslike>
-          <div>
+          <MaxMinFeelslike>
             <div>
-              <FiArrowDown color="#d8775a" />
-              <span>16℃</span>
+              <div>
+                <FiArrowDown color="#d8775a" />
+                <span>16℃</span>
+              </div>
+              <div>
+                <FiArrowUp color="#d8775a" />
+                <span>12℃</span>
+              </div>
+            </div>
+            <Feelslike>
+              <span>Sensação</span>
+              <span>19℃</span>
+            </Feelslike>
+          </MaxMinFeelslike>
+          <WindHumid>
+            <div>
+              <div>
+                <span>Vento</span>
+                <span>18km/h</span>
+              </div>
             </div>
             <div>
-              <FiArrowUp color="#d8775a" />
-              <span>12℃</span>
+              <span>Humidade</span>
+              <span>89%</span>
             </div>
-          </div>
-          <Feelslike>
-            <span>Sensação</span>
-            <span>19℃</span>
-          </Feelslike>
-        </MaxMinFeelslike>
-        <WindHumid>
-          <div>
+          </WindHumid>
+          <Line />
+          <Forecast>
             <div>
-              <span>Vento</span>
-              <span>18km/h</span>
+              <p>Terça</p>
+              <div>
+                <span>18℃</span>
+                <span>20℃</span>
+              </div>
             </div>
-          </div>
-          <div>
-            <span>Humidade</span>
-            <span>89%</span>
-          </div>
-        </WindHumid>
-        <Line />
-        <Forecast>
-          <div>
-            <p>Terça</p>
             <div>
-              <span>18℃</span>
-              <span>20℃</span>
+              <p>Quarta</p>
+              <div>
+                <span>18℃</span>
+                <span>20℃</span>
+              </div>
             </div>
-          </div>
-          <div>
-            <p>Quarta</p>
             <div>
-              <span>18℃</span>
-              <span>20℃</span>
+              <p>Quinta</p>
+              <div>
+                <span>18℃</span>
+                <span>20℃</span>
+              </div>
             </div>
-          </div>
-          <div>
-            <p>Quinta</p>
             <div>
-              <span>18℃</span>
-              <span>20℃</span>
+              <p>Sexta</p>
+              <div>
+                <span>18℃</span>
+                <span>20℃</span>
+              </div>
             </div>
-          </div>
-          <div>
-            <p>Sexta</p>
             <div>
-              <span>18℃</span>
-              <span>20℃</span>
+              <p>Sábado</p>
+              <div>
+                <span>18℃</span>
+                <span>20℃</span>
+              </div>
             </div>
-          </div>
-          <div>
-            <p>Sábado</p>
-            <div>
-              <span>18℃</span>
-              <span>20℃</span>
-            </div>
-          </div>
-        </Forecast>
-      </Details>
+          </Forecast>
+        </Details>
+      )}
+
       <Form>
         <input
           type="text"
           className="inputCity"
           placeholder="Insira aqui o nome da sua cidade"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyPress={handleSearchKeyPress}
         />
-        <button
-          type="button"
-          onClick={() => alert('teste')}
-          className="searchButton"
-        >
+        <button type="button" onClick={handleSearch} className="searchButton">
           <FiSearch color="#999" size={24} />
         </button>
       </Form>
@@ -174,19 +202,19 @@ const Home = () => {
               <section>
                 <strong>18℃</strong>
                 <strong>102℃</strong>
-                <strong>Rio de janeiro</strong>
+                <strong>Recife</strong>
               </section>
 
               <section>
                 <strong>18℃</strong>
                 <strong>102℃</strong>
-                <strong>Rio de janeiro</strong>
+                <strong>São Paulo</strong>
               </section>
 
               <section>
                 <strong>18℃</strong>
                 <strong>102℃</strong>
-                <strong>Rio de janeiro</strong>
+                <strong>Brasília</strong>
               </section>
             </div>
           </div>
@@ -201,19 +229,19 @@ const Home = () => {
               <section>
                 <strong>18℃</strong>
                 <strong>102℃</strong>
-                <strong>Rio de janeiro</strong>
+                <strong>Belem</strong>
               </section>
 
               <section>
                 <strong>18℃</strong>
                 <strong>102℃</strong>
-                <strong>Rio de janeiro</strong>
+                <strong>Salvador</strong>
               </section>
 
               <section>
                 <strong>18℃</strong>
                 <strong>102℃</strong>
-                <strong>Rio de janeiro</strong>
+                <strong>Curitiba</strong>
               </section>
             </div>
           </div>
